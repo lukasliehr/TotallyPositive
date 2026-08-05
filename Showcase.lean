a35a44b6c1e462c91dc9e1e919ad59d2f74a41b2
Lukas Liehr <lksliehr@gmail.com>
2026-08-06 00:24:09 +0200

    Initial commit

--- Showcase.lean
+++ Showcase.lean
@@ -4,39 +4,39 @@ open scoped ENNReal InnerProductSpace
 
 noncomputable section
 
-
+/-- Definition of a totally positive function -/
 def IsTP (g : ℝ → ℝ) : Prop :=
   g ≠ 0 ∧
   ∀ n : ℕ, ∀ a b : Fin n → ℝ,
     StrictMono a → StrictMono b →
     0 ≤ Matrix.det (Matrix.of fun i j => g (a i - b j))
 
-
+/-- Definition of a totally positive, integrable, continuous function -/
 def IsTPIntegrableContinuous (g : ℝ → ℝ) : Prop :=
   IsTP g ∧ Integrable g ∧ Continuous g
 
-
+/-- Definition of an element of the Gabor system -/
 def GaborAtom_Function
     (g : ℝ → ℝ) (α β : ℝ) (m n : ℤ) : ℝ → ℂ :=
   fun x =>
     Complex.exp (2 * Real.pi * Complex.I * β * n * x) *
       g (x - α * m)
 
-
+/-- Proof that GaborAtom_Function is an element of L²(ℝ) when g is a totally positive, integrable, continuous function -/
 lemma gaborAtom_memL2
     (g : ℝ → ℝ) (hg : IsTPIntegrableContinuous g)
     (α β : ℝ) (m n : ℤ) :
     MemLp (GaborAtom_Function g α β m n) 2 volume := by
   sorry
 
-
+/-- The Gabor atom as an actual element of L²(ℝ) -/
 def GaborAtom
     (g : ℝ → ℝ) (hg : IsTPIntegrableContinuous g)
     (α β : ℝ) (m n : ℤ) :
     Lp ℂ 2 (volume : Measure ℝ) :=
   (gaborAtom_memL2 g hg α β m n).toLp
 
-
+/-- Property that the Gabor system G(g, α, β) is a frame -/
 def IsGaborFrame
     (g : ℝ → ℝ) (hyp : IsTPIntegrableContinuous g)
     (α β : ℝ) : Prop :=
@@ -47,7 +47,7 @@ def IsGaborFrame
         ‖⟪f, GaborAtom g hyp α β m n⟫_ℂ‖ ^ 2
     A * ‖f‖ ^ 2 ≤ E ∧ E ≤ B * ‖f‖ ^ 2
 
-
+/-- Main result -/
 theorem TPFrameSet
     {α β : ℝ} (hyp1 : 0 < α) (hyp2 : 0 < β) (hyp3 : α * β < 1)
     (g : ℝ → ℝ) (hyp4 : IsTPIntegrableContinuous g) :
